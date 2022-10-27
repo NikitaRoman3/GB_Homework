73c9ecc086c2901be858973f3ad307429026fbca
Nikita <80348334+NikitaRoman3@users.noreply.github.com>
2022-10-27 20:49:48 +0300

    Add files via upload

---
+++ Контролькая работа_1/Решение задачи/Program.cs
@@ -0,0 +1,39 @@
+﻿void FillArray(string[] array)
+{
+    Console.WriteLine("Введите значение массива: ");
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        string text = Console.ReadLine() ?? "0";
+        array[i] = text;
+    }
+}
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+        Console.Write($"{array[i]} ");
+}
+
+void ChangeArray(string[] array, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            array2[count] = array[i];
+            count++;
+        }
+    }
+}
+
+Console.WriteLine("Введите размер массива: ");
+int size = int.Parse(Console.ReadLine() ?? "0");
+string[] array = new string[size];
+string[] copyarray = new string[size];
+FillArray(array);
+Console.WriteLine("Заданный массив:");
+PrintArray(array);
+Console.WriteLine("");
+ChangeArray(array, copyarray);
+Console.WriteLine("Новый массив с перенесенными значениями: ");
+PrintArray(copyarray);
